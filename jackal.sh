646677184903ba5f3c5b0a58c5090f57d97686f9
MrOctantis <zmudd@san.rr.com>
2016-11-25 19:19:27 -0800

    Added some basic functions Functions added: 	-v (display version) 	-h (display help) 	\? (display help when no option)

--- jackal.sh
+++ jackal.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 #########################
 
 # Jackal Network Monitoring Toolkit
-# @ArtificialAmateur
-# v .07
+# @ArtificialAmateur && @MrOctantis
 
 #########################
 
@@ -44,14 +43,26 @@ dX.    9Xb	.dXb    __                         __    dXb.     dXP     .Xb
                                `             '
 END
 
-while true; do
-echo $'\n[?] What module would you like to launch?' 
-echo $'  1) Category 1 \n  2) Category 2 \n  3) Category 3' 
-read modulec
-     case "$modulec" in
-       1 ) modules/foobar1.sh;;
-       2 ) modules/foobar2.sh;;
-       3 ) modules/foobar3.sh;;
-       * )  echo $'[-] Quitting script.\n' && break;;
-     esac 
+function display_help() {
+cat <<'END'
+usage: jackal-toolkit [OPTION]
+Jackal Network Monitoring Toolkit.
+
+  -v 		output version information and exit
+  -h 		display this help and exit
+END
+}
+
+version='0.0.7'
+while getopts ":vh" opt; do
+	case $opt in
+		v)
+			echo "jackal-toolkit version $version";;
+		h)
+			display_help;;
+		\?)
+			display_help;;
+	esac
+	did_something=true
 done
+if [ ! $did_something ]; then display_help; fi
